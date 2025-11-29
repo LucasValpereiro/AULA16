085e42d9f91254c954c52c369149f93a6bf61c09
LucasValpereiro <lucasvalpereiro@hotmail.com>
2025-11-29 11:44:10 -0300

    att js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const inputCep = document.getElementById('cep');
+const buscarBtn = document.getElementById('buscarBtn');
+const loading = document.getElementById('loading');
+const erro = document.getElementById('erro');
+const resultado = document.getElementById('resultado');
+const mensagemErro = document.getElementById('mensagemErro');
